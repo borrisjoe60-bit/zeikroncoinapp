b5374ba490f35f857179683333b5ef243bcf2fcf
borrisjoe60-bit <borrisjoe60@gmail.com>
2026-07-07 15:24:29 +0100

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,14 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
-
+import authRoutes from "./routes/auth";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
-
+app.use("/api/auth", authRoutes);
 app.get("/", (req, res) => {
   res.json({
     name: "Zeikroncoin API",
