e129af73ee2b8e58084c18acddd22853f71da2ea
borrisjoe60-bit <borrisjoe60@gmail.com>
2026-07-10 01:56:36 +0100

    Update hardhat.config.ts

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,19 +1,11 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import * as dotenv from "dotenv";
-
-dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
   networks: {
-    polygon: {
-      url: process.env.POLYGON_RPC_URL || "",
-      accounts: process.env.PRIVATE_KEY
-        ? [process.env.PRIVATE_KEY]
-        : []
-    }
-  }
+    hardhat: {},
+  },
 };
 
 export default config;
