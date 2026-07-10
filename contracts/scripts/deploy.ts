e4f36085306db67540d12124d672dc618b861795
borrisjoe60-bit <borrisjoe60@gmail.com>
2026-07-10 01:55:38 +0100

    Update deploy.ts

--- contracts/scripts/deploy.ts
+++ contracts/scripts/deploy.ts
@@ -1,12 +1,17 @@
 import { ethers } from "hardhat";
 
 async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contract with:", deployer.address);
+
   const Token = await ethers.getContractFactory("ZiekronToken");
-  const token = await Token.deploy(1000000000);
+
+  const token = await Token.deploy(deployer.address);
 
   await token.waitForDeployment();
 
-  console.log("ZeikronToken deployed to:", await token.getAddress());
+  console.log("Zeikroncoin deployed to:", await token.getAddress());
 }
 
 main().catch((error) => {
